8ff18f4b99b99cf4df869c77bc8fad68ed9baf6e
kerenn123 <estevo.santos@escola.pr.gov.br>
2026-08-19 14:46:16 +0000

    .

---
+++ objetos.js
@@ -0,0 +1,10 @@
+const listaCPFS = [1111111, 22222, 33333];
+
+const informacoesPessoa = ["nome", "Jose", "idade", 32, "CPF", "1111222333"];
+
+console.log (informacoesPessoa[1]);
+
+const objetoPessoa = {
+    idade: 32,
+    nome: "Jose",
+}
